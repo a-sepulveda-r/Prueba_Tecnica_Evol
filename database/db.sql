450d45353705a82cb98b54c3b223a9350054f368
a-sepulveda-r <ariel.sepulvedar@gmail.com>
2023-10-10 11:58:21 -0300

    update db.sql

--- database/db.sql
+++ database/db.sql
@@ -1,22 +1,40 @@
+DROP DATABASE IF EXISTS db_evol;
+
+
 CREATE DATABASE db_evol;
 
-CREATE TABLE Medidor (
-  codigo VARCHAR(255) PRIMARY KEY,
-  nombre VARCHAR(255) NOT NULL,
-  fechaCreacion DATE NOT NULL,
-  descripcion TEXT
-);
+\c db_evol;  -- Cambia a la base de datos recién creada.
 
+-- Crea las tablas Cliente y Medidor con las relaciones.
 
 CREATE TABLE Cliente (
   RUT VARCHAR(255) PRIMARY KEY,
   nombre VARCHAR(255) NOT NULL,
   direccion TEXT NOT NULL
 );
-ALTER TABLE Medidor
-ADD COLUMN cliente_id VARCHAR(255) REFERENCES Cliente(RUT);
-' 'ALTER TABLE Medidor
-ADD CONSTRAINT max_medidores_por_cliente
-CHECK (
-  (SELECT COUNT(*) FROM Medidor WHERE cliente_id = NEW.cliente_id) <= 3
+
+
+CREATE TABLE Medidor (
+  codigo VARCHAR(255) PRIMARY KEY,
+  nombre VARCHAR(255) NOT NULL,
+  fechaCreacion DATE NOT NULL,
+  descripcion TEXT,
+  cliente_id VARCHAR(255) REFERENCES Cliente(RUT)
 );
+
+
+-- Insertar datos en la tabla Cliente
+INSERT INTO Cliente (RUT, nombre, direccion)
+VALUES 
+  ('19.222.094-1', 'Ximena Ramirez', 'Francisco de Camar 87'),
+  ('1935543-9', 'Manuel', 'Santiago 8786'),
+  ('6.315.707-4', 'Guillermo', 'San Leon Grande 44'),
+  ('19187656', 'Manolito', 'Rungue 567');
+
+-- Insertar datos en la tabla Medidor
+INSERT INTO Medidor (codigo, nombre, fechaCreacion, descripcion, cliente_id)
+VALUES 
+  ('8050919jk', 'Medidor luz', '2022-09-07', 'Medir luz', '19.222.094-1'),
+  ('897867j', 'Medidor luz', '2022-09-07', 'Mide el agua', '1935543-9'),
+  ('805gt6', 'Medidor luz', '2018-09-07', 'Medir luz', '6.315.707-4'),
+  ('5655', 'Medidor agua', '2018-09-07', 'Mide el agua', '19187656');
